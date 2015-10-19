6ac8a5eb06beeb1946bdeff930a1b9f69cb4bc1f
Daniel Streit <daniel.streit@mautilus.com>
2015-10-19 09:50:43 +0200

    Version increase to 2.0.217
    Version increase to 2.0.217

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 CONFIG = {
     locale: 'EN',
-    versionSDK: "2.0.187 [07.07.2015]",  // SDK version  (format: X.Y.SDK_SVN_Revision_number)
+    versionSDK: "2.0.217 [19.10.2015]",  // SDK version  (format: X.Y.SDK_SVN_Revision_number)
     version: "1.0.0 [18.10.2013]", // application version - put your own
     developer: {
 		debug: true,
