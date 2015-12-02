afeb76e71cfedca6f4774571d4c894d45bba8621
Daniel Streit <daniel.streit@mautilus.com>
2015-12-02 11:00:35 +0100

    Added support for HbbTV
    Added support for HbbTV

---
+++ js/core/device/hbbtv.js
@@ -0,0 +1,285 @@
+/**
+ * Mautilus, s.r.o., 2013
+ * This file is part of MAUTILUS SMART TV SDK
+ *
+ * Questions and comments should be directed to info@mautilus.com
+ *
+ * ======================================================================
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * ===================
+ */
+
+/**
+ * HbbTV device, overrides Device
+ *
+ * @author Mautilus s.r.o.
+ * @class Device_Hbbtv
+ * @extends Device
+ */
+
+Device_Hbbtv = (function (Events) {
+	var Device_Hbbtv = {
+		isHbbTV: true,
+		isFireTVPlugin : function() { return navigator.userAgent.indexOf('firetv-firefox-plugin') > -1 ? true : false; }
+	};
+
+	$.extend(true, Device_Hbbtv, Events, {
+		/**
+		 * @inheritdoc Device#init
+		 */
+		init: function (callback, scope) {
+			// Hbbtv APPLICATION MANAGER AND CONFIGURATION
+			this.OIPF_CAP = this.loadObject('oipfCap', {
+				type: 'application/oipfCapabilities',
+				style: "position: absolute; left: 0; top: 0; width: 0; height: 0"
+			});
+			this.OIPF_APP_MAN = this.loadObject('oipfAppMan', {type: 'application/oipfApplicationManager'});
+			this.OIPF_CONFIG = this.loadObject('oipfConfig', {type: 'application/oipfConfiguration'});
+
+			// init APP
+			try {
+				this.APP = this.OIPF_APP_MAN.getOwnerApplication(document);
+				this.APP.show();
+				this.APP.activate();
+			} catch (e) {
+				// ignore e.g: SamsungTV
+				console.error("bad show or active app by oipfApplicationManager: " + e);
+			}
+
+			// register keyset
+			// var keySet = this.APP.privateData.keyset;
+			// var mask = keySet.RED | keySet.GREEN | keySet.YELLOW | keySet.BLUE | keySet.NAVIGATION | keySet.VCR | keySet.NUMERIC
+			var mask = 0x33F;
+			this._setKeyset(mask);
+
+			// override default modules
+			this.override();
+
+			this.setKeys();
+
+			if (callback) {
+				callback.call(scope || this);
+			}
+		},
+
+		/**
+		 * To set keyset
+		 * @private
+		 * @param {Number} mask
+		 */
+		_setKeyset: function (mask) {
+			// for HbbTV 0.5
+			try {
+				this.OIPF_CONFIG.keyset.value = mask;
+			} catch (e) {
+				console.error("bad register keys by oipfConfiguration 1: " + e);
+			}
+
+			try {
+				this.OIPF_CONFIG.keyset.setValue(mask);
+			} catch (e) {
+				console.error("bad register keys by oipfConfiguration 2: " + e);
+			}
+
+			// for HbbTV 1.0
+			try {
+				this.APP.privateData.keyset.setValue(mask);
+				this.APP.privateData.keyset.value = mask;
+			} catch (e2) {
+				console.error("bad register keys: " + e);
+			}
+		},
+
+		/**
+		 * @inheritdoc Device#exit
+		 */
+		exit: function (dvb) {
+			try {
+				this.APP.destroyApplication();
+				return; // destroyApplication() can throw exception, ignore it
+			} catch (e) {
+				window.close();
+			}
+		},
+
+		/**
+		 * @inheritdoc Device#getFirmware
+		 */
+		getFirmware: function () {
+			// @todo
+			return "";
+		},
+		/**
+		 * @inheritdoc Device#getUID
+		 */
+		getUID: function () {
+			// @todo
+			return "";
+		},
+		/**
+		 * @inheritdoc Device#getIP
+		 */
+		getIP: function () {
+			// @todo
+			return "";
+		},
+		/**
+		 * @inheritdoc Device#getDeviceName
+		 */
+		getDeviceName: function (stripSpaces) {
+			// @todo
+			name = navigator.userAgent.match(/hbbtv\S*\s\([^\(]*\)/i);   //e.g: HbbTV/1.2.1 (;;;;;)
+			return name;
+		},
+		/**
+		 * @inheritdoc Device#getCountry
+		 */
+		getCountry: function () {
+			// @todo
+			var country = "";
+			return country.toLowerCase();
+		},
+		/**
+		 * @inheritdoc Device#getLanguage
+		 */
+		getLanguage: function () {
+			// @todo
+			var language = "";
+			return language;
+		},
+		/**
+		 * @inheritdoc Device#getDate
+		 */
+		getDate: function () {
+			return new Date();
+		},
+		/**
+		 * @inheritdoc Device#getTimeZoneOffset
+		 */
+		getTimeZoneOffset: function () {
+			// @todo
+			return "";
+		},
+		/**
+		 * @inheritdoc Device#checkNetworkConnection
+		 */
+		checkNetworkConnection: function (callback, scope) {
+			// @todo
+		},
+		/**
+		 * Initialize key codes
+		 *
+		 * @private
+		 */
+		setKeys: function () {
+			// @todo
+			Control.setKeys({
+				LEFT: KeyEvent.VK_LEFT,
+				RIGHT: KeyEvent.VK_RIGHT,
+				UP: KeyEvent.VK_UP,
+				DOWN: KeyEvent.VK_DOWN,
+				ENTER: KeyEvent.VK_ENTER,
+				RETURN: KeyEvent.VK_BACK,
+				ZERO: KeyEvent.VK_0,
+				ONE: KeyEvent.VK_1,
+				TWO: KeyEvent.VK_2,
+				THREE: KeyEvent.VK_3,
+				FOUR: KeyEvent.VK_4,
+				FIVE: KeyEvent.VK_5,
+				SIX: KeyEvent.VK_6,
+				SEVEN: KeyEvent.VK_7,
+				EIGHT: KeyEvent.VK_8,
+				NINE: KeyEvent.VK_9,
+				RED: KeyEvent.VK_RED,
+				GREEN: KeyEvent.VK_GREEN,
+				YELLOW: KeyEvent.VK_YELLOW,
+				BLUE: KeyEvent.VK_BLUE,
+				PLAY: KeyEvent.VK_PLAY,
+				PAUSE: KeyEvent.VK_PAUSE,
+				STOP: KeyEvent.VK_STOP,
+				REC: KeyEvent.VK_RECORD,
+				FF: KeyEvent.VK_FAST_FWD,
+				RW: KeyEvent.VK_REWIND,
+				TOOLS: -1,
+				PUP: -1,
+				PDOWN: -1,
+				CHLIST: -1,
+				PRECH: -1,
+				TXTMIX: -1,
+				FAVCH: -1,
+				EXIT: -1,
+				INFO: -1
+			});
+		},
+		/**
+		 * Override default modules
+		 *
+		 * @private
+		 */
+		override: function () {
+			if (typeof Device_HbbTV_Player !== 'undefined' && Player) {
+				Player = $.extend(true, Player, Device_HbbTV_Player);
+			}
+		},
+		/**
+		 * Load specific JS library or file
+		 *
+		 * @private
+		 * @param {String} src
+		 */
+		loadJS: function (src) {
+			var s = document.createElement('script');
+			document.head.appendChild(s);
+			s.src = src;
+		},
+		/**
+		 * Load specific OBJECT
+		 *
+		 * @private
+		 * @param {String} id
+		 * @param {String|Object} type - type or object of attributes
+		 */
+		loadObject: function (id, type) {
+			var objs = document.getElementsByTagName('object');
+
+			if (objs) {
+				for (var i in objs) {
+					if (objs[i] && objs[i].id === id) {
+						return objs[i];
+					}
+				}
+			}
+
+			var obj = document.createElement('object');
+			obj.id = id;
+			if (typeof type == 'string') {
+				obj.setAttribute('type', type);
+			}
+			else if (typeof type == 'object') {
+				for (var key in type) {
+					if (type.hasOwnProperty(key)) {
+						obj.setAttribute(key, type[key]);
+					}
+				}
+			}
+
+			document.body.appendChild(obj);
+
+			return obj;
+		}
+	});
+
+	if (typeof document.head === 'undefined') {
+		document.head = document.getElementsByTagName('head')[0];
+	}
+
+	if (typeof document.body === 'undefined') {
+		document.body = document.getElementsByTagName('body')[0];
+	}
+
+	return Device_Hbbtv;
+
+})(Events);
---
+++ js/core/device/hbbtv/player.js
@@ -0,0 +1,301 @@
+/**
+ * Mautilus, s.r.o., 2013
+ * This file is part of MAUTILUS SMART TV SDK
+ *
+ * Questions and comments should be directed to info@mautilus.com
+ *
+ * ======================================================================
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * ===================
+ */
+
+/**
+ * HbbTV Player class
+ *
+ * @author Mautilus s.r.o.
+ * @class Device_HbbTV_Player
+ * @extends Player
+ */
+
+Device_HbbTV_Player = (function (Events) {
+	var Device_HbbTV_Player = {};
+
+	$.extend(true, Device_HbbTV_Player, {
+		/**
+		 * @inheritdoc Player#initNative
+		 */
+		initNative: function () {
+			var scope = this;
+
+			$("body").append('<div id="vidcontainer" style="position: absolute; background: transparent; left: 0px; top: 0px; width: 1280px; height: 720px;"></div>');
+			this.$videoContainer = $('#vidcontainer');
+
+			this.createPlayer('video/broadcast');
+
+			clearTimeout(this.ticker);
+			this.ticker = setInterval(function () {
+				scope.tick();
+			}, 500);
+		},
+
+		/**
+		 * @inheritdoc Player#deinitNative
+		 */
+		deinitNative: function () {
+			console.log('[Device_HbbTV_Player] deinitNative process: '+ this.PLAYER);
+			if (!this.PLAYER) {
+				return;
+			}
+			console.log('[Device_HbbTV_Player] deinitNative start');
+			try {
+				this.PLAYER.stop();
+			} catch(e) {
+				console.log("error PLAYER.stop(): " + e);
+			}
+			this.PLAYER.data = '';
+			try {
+				this.PLAYER.release();
+			} catch(e) {
+				console.log("error PLAYER.release(): " + e);
+			}
+			//this.PLAYER.parentNode.removeChild(this.PLAYER); -- not working in FireTV
+			this.$videoContainer[0].innerHTML = '';
+			this.PLAYER = false;
+			this.type = '';
+			console.log('[Device_HbbTV_Player] deinitNative end');
+		},
+
+		/**
+		 * @private
+		 */
+		tick: function () {
+			if (this.url && this.PLAYER && typeof this.PLAYER.playTime !== 'undefined') {
+				if (!this.duration && this.PLAYER.playTime) {
+					this.onDurationChange(this.PLAYER.playTime);
+				}
+
+				var pos = Math.round(this.PLAYER.playPosition >> 0);
+
+				if (pos && pos !== this.currentTime) {
+					this.onTimeUpdate(pos);
+				}
+			}
+		},
+
+		/**
+		 * @private
+		 * @param {String} type - 'video/broadcast' or 'video/mp4'
+		 */
+		createPlayer: function (type) {
+			console.log("[Device_HbbTV_Player] createPlayer xyz: " + type);
+			if (this.PLAYER) {
+				this.deinitNative();
+			}
+			this.type = type;
+
+			var strVideo;
+			if(Device.isFireTVPlugin()) {
+				strVideo = '<object id="video" type="" width="1280" height="720" style="z-index: 1; position: absolute; left: 0px; top: 0px; width: 1280px; height: 720px; visibility: hidden; outline: transparent"></object>';
+				this.$videoContainer[0].innerHTML = strVideo;
+				this.PLAYER = document.getElementById('video');
+				this.PLAYER.setAttribute('type', type);  // fireTV
+			} else {
+				// Samsung
+				strVideo = '<object id="video" type="'+type+'" width="1280" height="720" style="z-index: 1; position: absolute; left: 0px; top: 0px; width: 1280px; height: 720px; visibility: hidden; outline: transparent"></object>';
+				this.$videoContainer[0].innerHTML = strVideo;
+				this.PLAYER = document.getElementById('video');
+			}
+
+			// style can be set only after object is inserted to DOM not sooner! - for FireTV
+//			obj.style.position = 'absolute';
+//			obj.style.width = this.config.width + 'px';
+//			obj.style.height = this.config.height + 'px';
+//			obj.style.left = this.config.left + 'px';
+//			obj.style.top = this.config.top + 'px';
+//			obj.style.outline = 'transparent';
+//			obj.style.zIndex = '0';
+//			obj.style.visibility = 'hidden';
+
+//			this.PLAYER.type = 'video/mp4';    // fireTV
+//			console.log("[Device_HbbTV_Player] setAttribute");
+
+
+			if(type === 'video/broadcast') {
+				this.initBroadcastPlayer();
+			} else if(type === 'video/mp4') {
+				this.initAVPlayer();
+			}
+		},
+
+		initBroadcastPlayer: function() {
+			console.log('initBroadcastPlayer');
+			try {
+				this.PLAYER.bindToCurrentChannel();
+			} catch (e) {
+				console.error('error bindToCurrentChannel: ' + e);  // ignore
+			}
+			try {
+				this.PLAYER.setFullScreen(true);
+			} catch (e) {
+				console.error('error setFullScreen: ' + e);  // ignore
+			}
+		},
+
+		initAVPlayer: function() {
+			var scope = this;
+			this.PLAYER.onPlayStateChange = function (state, error) {
+				scope.onNativePlayStateChange(state, error);
+			};
+
+			this.PLAYER.onPlaySpeedChanged = function () {
+			};
+			this.PLAYER.onPlayPositionChanged = function () {
+			};
+		},
+
+		/**
+		 * @inheritdoc Player#native
+		 */
+		native: function (cmd, attrs) {
+			console.log("[Device_HbbTV_Player] cmd: " + cmd);
+			var scope = this, url;
+
+			if (cmd === 'play') {
+				if (attrs && attrs.url) {
+					url = this.url;
+
+					console.network('PLAYER', this.url);
+					this.createPlayer('video/mp4');
+					this.PLAYER.style.visibility = 'visible';
+					this.PLAYER.data = url;
+				}
+
+				this.PLAYER.play(1);
+
+				if (attrs && attrs.position) {
+					this._seekOnPlay = attrs.position;
+				}
+
+			} else if (cmd === 'pause') {
+				return this.PLAYER.play(0);
+
+			} else if (cmd === 'stop') {
+				this.PLAYER.stop();
+				this.PLAYER.data = '';
+
+			} else if (cmd === 'seek') {
+				return this.PLAYER.seek(attrs.position);
+
+			} else if (cmd === 'playbackSpeed') {
+				return this.PLAYER.play(attrs.speed);
+
+			} else if (cmd === 'show') {
+				if(!this.PLAYER) {
+					return;
+				}
+
+				this.width = attrs.width || this.width;
+				this.height = attrs.height || this.height;
+				this.top = (typeof attrs.top !== 'undefined' ? attrs.top : this.top);
+				this.left = (typeof attrs.left !== 'undefined' ? attrs.left : this.left);
+
+				this.PLAYER.style.visibility = 'visible';
+				this.PLAYER.style.width = this.width + 'px';
+				this.PLAYER.style.height = this.height + 'px';
+				this.PLAYER.style.top = this.top + 'px';
+				this.PLAYER.style.left = this.left + 'px';
+
+			} else if (cmd === 'hide') {
+				this.PLAYER.style.visibility = 'hidden';
+				//this.$el.css('visibility', 'hidden');
+
+			} else if (cmd === 'setVideoDimensions') {
+				// @todo
+
+			} else if (cmd === 'audioTrack') {
+				// @todo
+			}
+		},
+
+		/**
+		 * @private
+		 */
+		getESN: function () {
+			// @todo
+		},
+
+		/**
+		 * @private
+		 */
+		onNativePlayStateChange: function (state, error) {
+			var state = this.PLAYER.playState;
+			console.log("[Device_HbbTV_Player] onNativePlayStateChange:: " + state);
+
+			if (state == 0) {
+				//stopped
+			} else if (state == 1) {
+				// playing
+				this.state(this.STATE_PLAYING);
+
+				if (this._seekOnPlay) {
+					this.PLAYER.seek(this._seekOnPlay);
+					this._seekOnPlay = 0;
+				}
+
+			} else if (state == 2) {
+				// paused
+				this.state(this.STATE_PAUSED);
+			} else if (state == 3) {
+				// connecting
+				this.state(this.STATE_BUFFERING);
+			} else if (state == 4) {
+				// buffering
+				this.state(this.STATE_BUFFERING);
+			} else if (state == 5) {
+				// finished
+				this.onEnd();
+			} else if (state == 6) {
+				// error
+				var errorno = isNaN(this.PLAYER.error) ? "" : this.PLAYER.error+'';
+				var errormsg = '';
+				switch(errorno) {
+					case '0':
+						errormsg = "A/V format not supported";
+						break;
+					case '1':
+						errormsg = "cannot connect to server or lost connection";
+						break;
+					case '2':
+						errormsg = "unidentified error";
+						break;
+						// 3-6 are defined in OIPF annex B
+					case '3':
+						errormsg = "insufficient resources.";
+						break;
+					case '4':
+						errormsg = "content corrupt or invalid.";
+						break;
+					case '5':
+						errormsg = "content not available.";
+						break;
+					case '6':
+						errormsg = "content not available at given position.";
+						break;
+					    // defined in HbbTV for download content
+					case '7':
+						errormsg = "content blocked due to parental control.";
+						break;
+				}
+				console.log("error no: " + errorno + ', msg: ' + errormsg);
+				this.onError(0, errorno);
+			}
+			//console.log("[onPlayStateChange] " + state + ", error: " + error);
+		}
+	});
+
+	return Device_HbbTV_Player;
+
+})(Events);
