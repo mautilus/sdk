abee7b3040adddf4fe4b4206930df0bb2d12ea02
Radim Buchtela <radim.buchtela@mautilus.com>
2017-02-17 13:59:15 +0100

    Version increase to 2.1.292

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 CONFIG = {
-    locale: 'EN',
-    versionSDK: '2.0.230 [02.05.2016]',  // SDK version  (format: X.Y.SDK_SVN_Revision_number)
-    version: '1.0.0 [18.10.2013]', // application version - put your own
-    appName: 'applicationName',
-    developer: {
+	locale: 'EN',
+	versionSDK: '2.1.292 [17.02.2017]',  // SDK version  (format: X.Y.SDK_SVN_Revision_number)
+	version: '1.0.0 [18.10.2013]', // application version - put your own
+	appName: 'applicationName',
+	developer: {
 		debug: true,
 		active: true,
 		console: null
-    },
-    player: {
+	},
+	player: {
 		muted: true
-    },
-    ajax: {
+	},
+	ajax: {
 		timeout: 60000
-    },
-    keyboard: {
-    	oneLayout: false
-    },
-    GA: {
-      account: '', // account number for Google Analytics
-      ssl: true
-    }
+	},
+	keyboard: {
+		oneLayout: false
+	},
+	GA: {
+		account: '', // account number for Google Analytics
+		ssl: true
+	}
 };
